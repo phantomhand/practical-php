6d32e5e6bdd093c15896583af37a79d9f4375696
Frank Aveni <frank@phantomhand.com>
2019-02-26 00:59:25 -0500

    Add completed practice file
    Add completed practice file for Lesson 16: Arithmetic Operators

--- 16_ArithmeticOperators/practice.php
+++ 16_ArithmeticOperators/practice.php
@@ -1,17 +1,18 @@
 <?php
 	
 	// Constants
-
+    define("TITLE", "Arithmetic Operators");
 	
 	// Custom Variables
-
+    $myName = "Frank Aveni";
+    $lessonNum = 16;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,7 +21,7 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lessonNum; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
@@ -29,27 +30,47 @@
 				
 				<h3>Addition <code>+</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+                    $b = 19;
+                    $c = ($a + $b);
+                
+                    echo "$a + $b = $c";
 				?>
 				
 				<h3>Subtraction <code>-</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+                    $b = 19;
+                    $c = ($a - $b);
+                
+                    echo "$a - $b = $c";
 				?>
 				
 				<h3>Multiplication <code>*</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+                    $b = 19;
+                    $c = ($a * $b);
+                
+                    echo "$a &times; $b = $c";
 				?>
 				
 				<h3>Division <code>/</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+                    $b = 19;
+                    $c = ($a / $b);
+                
+                    echo "$a &#247; $b = $c";
 				?>
 				
 				<h3>Modulus <code>%</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+                    $b = 19;
+                    $c = ($a % $b);
+                
+                    echo "$a % $b = $c";
 				?>
 				
 			</div><!-- end sandbox -->
@@ -58,7 +79,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y'); ?> - <?php echo $myName; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
