481f07ff829358763bcfe0e3a4fdd5588b0345c4
Frank Aveni <frank@phantomhand.com>
2019-02-27 01:40:17 -0500

    Add completed practice file
    Add completed practice file for Lesson 18: Assignment Operators

--- 18_AssignmentOperators/practice.php
+++ 18_AssignmentOperators/practice.php
@@ -1,17 +1,18 @@
 <?php
 	
 	// Constants
-
+    define("TITLE", "Assignment Operators");
 	
 	// Custom Variables
-
+    $myName = "Frank Aveni";
+    $lessonNum = 18;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,7 +21,7 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lessonNum; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
@@ -29,32 +30,56 @@
 				
 				<h3>Addition Assignment Operator <code>+=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+                    $b = 50;
+                    $a += $b; // adds $b and $a together and re-assigns the value to $a
+                        
+                    echo $a;
 				?>
 				
 				<h3>Subtraction Assignment Operator <code>-=</code></h3>
 				<?php
-					// your code here
+				    $a = 30;
+                    $b = 50;
+                    $a -= $b; // subtracts $b from $a and re-assigns the value to $a
+                        
+                    echo $a;
 				?>
 				
 				<h3>Multiplication Assignment Operator <code>*=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+                    $b = 50;
+                    $a *= $b; // multiplies $b x $a and re-assigns the value to $a
+                        
+                    echo $a;
 				?>
 				
 				<h3>Division Assignment Operator <code>/=</code></h3>
 				<?php
-					// your code here
+				    $a = 30;
+                    $b = 50;
+                    $a /= $b; // divides $a by $b and re-assigns the value to $a
+                        
+                    echo $a;
 				?>
 				
 				<h3>Modulus Assignment Operator <code>%=</code></h3>
 				<?php
-					// your code here
+				    $a = 30;
+                    $b = 50;
+                    $a %= $b; // finds the remainder of $a divided by $b and re-assigns the value to $a
+                        
+                    echo $a;
 				?>
 				
 				<h3>Concatenation Assignment Operator <code>.=</code></h3>
 				<?php
-					// your code here
+				    $a = "Hola, ";
+                    $b = "Mi Amigos!";
+                    $a .= $b; // concatenates $b to $a and re-assigns the value to $a
+                        
+                    echo $a;
 				?>
 				
 			</div><!-- end sandbox -->
@@ -63,7 +88,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
+			<small>&copy;<?php echo date('Y'); ?> - <?php echo $myName; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
