b74c0d76a0febb388ae54208f6292b05245bca9a
Frank Aveni <frank@phantomhand.com>
2019-04-18 23:50:16 -0400

    Add initial files for project site
    Add Student folder with index file, includes folder (header, footer, nav, and arrays files), and img folder. Completed through Lesson 34: Templating a Global Header & Footer.

---
+++ 26_Final/Student/includes/footer.php
@@ -0,0 +1,40 @@
+ 
+        <div id="footer" class="cf">
+              
+              <div class="column three">
+                  <strong>Phone</strong>
+                  (123) 456-7890
+              </div><!--column 1-->
+              
+              <div class="column three">
+                  <strong>Location</strong>
+                  3456 Main Street<br>
+                  Someplace, USA
+              </div><!--column 2-->
+              
+              <div class="column three last">
+                 <strong>Hours</strong>
+                  <em>Monday - Thursday</em><br>
+                  1:00pm-9:00pm<br><br>
+                  
+                  <em>Friday - Saturday</em><br>
+                  4:00pm-11:00pm<br><br>
+                  
+                  <em>Sunday</em><br>
+                  Closed<br>               
+              </div><!--column 3-->
+               
+           </div><!--footer-->
+           
+           <small>&copy; 2019 Frank Aveni</small>
+           
+        </div><!--content-->
+        
+    </div><!--wrapper-->
+    
+    <div class="copyright-info">
+        <?php include('../includes/copyright.php') ?>
+    </div><!--copyright-info-->
+     
+ </body> 
+</html>
