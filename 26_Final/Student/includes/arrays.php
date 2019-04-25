1d963797e8f574ff2ca9773bc0557e90c36de49e
phantomhand <frankaveni@gmail.com>
2019-04-24 23:09:49 -0400

    Add files for Lesson 37: Code a 'Menu' Array & Template

--- 26_Final/Student/includes/arrays.php
+++ 26_Final/Student/includes/arrays.php
@@ -52,4 +52,44 @@
                     )
         
                 );
+
+    // Menu Items
+    
+    $menuItems = array(
+                    
+                    "club-sandwich"  => array(
+                        
+                        title       => "Club Sandwich",
+                        price       => 11,
+                        blurb       => "Bacon ipsum dolor amet short ribs venison tongue short loin cupim. Flank biltong salami, short loin boudin andouille pig porchetta buffalo hamburger beef turkey ribeye burgdoggen. Swine ham hock porchetta sausage. Shankle spare ribs capicola andouille.",
+                        drink       => "Club Soda"
+                    ),
+                
+                     "dill-salmon"  => array(
+                        
+                        title       => "Dill Salmon",
+                        price       => 18,
+                        blurb       => "Spicy jalapeno bacon ipsum dolor amet quis prosciutto duis chicken bacon enim tenderloin ullamco id ball tip in lorem. Reprehenderit cow proident tempor spare ribs dolore. Meatloaf consequat irure, ham hock shankle proident capicola ex ribeye.",
+                        drink       => "Boxed Wine"
+                    ),
+        
+                    "super-salad"  => array(
+                        
+                        title       => "Super Salad<sup>&reg;</sup>",
+                        price       => 12,
+                        blurb       => "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Parsley shallot courgette tatsoi pea sprouts fava bean. Dandelion cucumber earthnut pea peanut soko zucchini",
+                        drink       => "Big Glass of Water"
+                    ),
+        
+                    "mexican-barbacoa"  => array(
+                        
+                        title       => "Mexican Barbacoa",
+                        price       => 16,
+                        blurb       => "Cupcake ipsum dolor sit amet pastry cheesecake jelly. Toffee donut dragée lemon drops gummi bears lollipop soufflé caramels sugar plum. Carrot cake liquorice toffee cookie cookie. Pudding tootsie roll soufflé chocolate bar chupa chups jelly-o topping fruitcake.",
+                        drink       => "Beer with Lime"
+                    ),
+        
+        
+                );
+
 ?>
---
+++ 26_Final/Student/menu.php
@@ -0,0 +1,29 @@
+<?php 
+    define("TITLE", "Menu | Franklin's Fine Dining");
+
+    include('includes/header.php');
+?>
+
+    <div id="menu-items">
+        
+        <h1>Our Delicious Menu</h1>
+        <p>Like our team, our menu is very small &mdash; but man, does it ever pack a punch!</p>
+        <p><em>Click any menu item to learn more about it.</em></p>
+        
+        <hr>
+        
+        <ul>
+            <?php foreach ($menuItems as $dish => $item) { ?>
+            
+            <li><a href="dish.php?item=<?php echo $dish; ?>"><?php echo $item[title]; ?></a> – <sup>$</sup><?php echo $item[price]; ?></li>
+            
+            <?php } ?>
+            
+        </ul>
+        
+        <hr>
+        
+    </div><!--menu-items-->
+
+<?php include(includes/footer.php); ?>
+
