651559594199b28311157e3313cd99608faaf462
phantomhand <frankaveni@gmail.com>
2019-04-28 02:09:16 -0400

    Add Contact page - Lesson 40: Code a Simple Contact Form

---
+++ 26_Final/Student/contact.php
@@ -0,0 +1,36 @@
+<?php 
+
+    define("TITLE", "Contact | Franklin's Fine Dining");
+
+    include('includes/header.php');
+
+?>
+
+<div id="contact">
+    
+    <hr>
+    
+    <h1>Get in touch with us</h1>
+    
+    <form method="post" action="" id="contact-form">
+        
+        <label for="name">Your Name</label>    
+        <input type="text" id="name" name="name">
+        
+        <label for="email">Your Email</label>    
+        <input type="text" id="email" name="email">
+        
+        <label for="message">Your Message</label>
+        <textarea id="message" name="message"></textarea>
+        
+        <input type="checkbox" id="subscribe" name="subscribe" value="Subscribe">
+        <label for="subscribe">Subscribe to newsletter</label>
+        
+        <input type="submit" class="button next" name="contact_submit" value="Send Message">    
+        
+    </form>
+    
+</div><!--contact-->
+
+
+<?php include('includes/footer.php'); ?>
