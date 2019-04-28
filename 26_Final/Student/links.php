b1bc0a883ebe5405ebe8914ed58129ad417f219c
phantomhand <frankaveni@gmail.com>
2019-04-28 15:19:36 -0400

    Add Links file with course credits and certificate of completion

---
+++ 26_Final/Student/links.php
@@ -0,0 +1,23 @@
+<?php 
+    define("TITLE", "Links | Franklin's Fine Dining");
+    
+    include('includes/header.php'); 
+?>
+
+    <div id="philosophy">
+        <hr>
+        
+        <h1>Practical PHP</h1>
+        
+        <p><strong>Credit:</strong><br>
+        This site was made as part of Brad Hussey's Udemy course<br>
+        <strong>&ldquo;<a href="https://www.udemy.com/code-dynamic-websites/">Practical PHP: Master the Basics and Code Dynamic Websites</a>&rdquo;</strong><br>
+        <br>
+        <strong>View the <a href="https://www.udemy.com/certificate/UC-LZSQC0Y7/">Certificate of Completion</strong></a></p>
+        
+        <p><strong>GitHub repo:</strong> <a href="https://github.com/phantomhand/practical-php">github.com/phantomhand/practical-php</a></p>
+        
+        <hr>
+    </div><!--links-->
+
+<?php include('includes/footer.php'); ?>
